78efeb9c0d7ee82fbbfed5ace60ea2308ed31071
jgarcia <gsusli@hotmail.com>
2012-05-21 10:34:38 +0200

    PRIMER COMMIT.

---
+++ LILO/src/manola.java
@@ -0,0 +1,14 @@
+
+public class manola {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("nos hacemos unas pajillas");
+
+	}
+
+}
